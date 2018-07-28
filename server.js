be95cb7895662f66f90f47e315bf4057144089cc
ximbong91023 <ximbong91023@yahoo.com>
2018-07-28 15:42:26 +0300

    Add displayData path

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express'),
       path = require('path'),
-      app = express();  
+      app = express();
 
-var publicPath = path.join(__dirname, "./public");
 var port = process.env.PORT || 3000;
+
+var displayDataPath = path.join(__dirname, "./displayData");
+var publicPath = path.join(__dirname, "./public");
+
 app.use(express.static(publicPath));
+app.use("/display",express.static(displayDataPath));
 
 app.listen(port, (req, res) => {
     console.log('Server is running at port ' + port);
