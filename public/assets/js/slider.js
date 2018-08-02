62eba24ea2b39fd4d4cb58eafde0f16f6a32ef25
tripheo0412 <tripheo0412@gmail.com>
2018-08-02 21:49:08 +0300

    fix ui

---
+++ public/assets/js/slider.js
@@ -0,0 +1,9 @@
+var btn =  document.getElementsByName('radio-btn');
+
+var imgArray = ["1.png","2.gif","3.png"];
+
+for ( let i =0 ; i < btn.length; i++) {
+    btn[i].onclick = function(){
+        document.querySelector(".featured-images").style.backgroundImage = `url('../public/images/${imgArray[i]}')`
+    }
+}
