fdbc5921cbca19ecbef6c7240162a597e9c99d40
tripheo0412 <tripheo0412@gmail.com>
2018-08-13 18:19:30 +0300

    clean up home page

--- public/other-sites/assets/js/slider.js
+++ public/other-sites/assets/js/slider.js
@@ -4,6 +4,6 @@ var imgArray = ["1.png","2.gif","3.png"];
 
 for ( let i =0 ; i < btn.length; i++) {
     btn[i].onclick = function(){
-        document.querySelector(".featured-images").style.backgroundImage = `url('/images/${imgArray[i]}')`
+        document.querySelector(".featured-images").style.backgroundImage = `url('/public/other-sites/images/${imgArray[i]}')`
     }
 }
