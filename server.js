b605643b675052992437787012eeec24989470ed
Hai Truong <ximbong91023@yahoo.com>
2018-08-20 20:51:08 +0300

    Enchance application routes

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-var express = require('express'),
-      path = require('path'),
-      app = express();
+const express = require("express"),
+	path = require("path"),
+	app = express();
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-var displayDataPath = path.join(__dirname, "./displayData");
-var publicPath = path.join(__dirname, "./public");
-var homePath =  path.join(__dirname, "./public/home.html");
-var learnmorePath = path.join(__dirname, "./public/learnMore.html")
+const homePath = path.join(__dirname, "./public/other/home.html");
+const projectPath = path.join(__dirname, "./public");
+const learnMorePath = path.join(__dirname, "./public/other/learn-more.html");
 
-app.use(express.static(publicPath));
-app.use("/display", express.static(displayDataPath));
-app.use("/home", express.static(homePath));
-app.use("/learn-more", express.static(learnmorePath));
+app.get("/", function(req, res) {
+	res.sendFile(homePath);
+});
 
-app.get("/data/:stationName", function(req,res){
-  const stationName = req.params.stationName;
-  const data = require(`./data/historical-data/${stationName}`);
+app.use("/more", express.static(learnMorePath));
+app.use("/demo", express.static(projectPath));
 
-  res.send(data);
-})
+app.get("/data/:stationName", function(req, res) {
+	const stationName = req.params.stationName;
+	const data = require(`./data/historical-data/${stationName}`);
+
+	res.send(data);
+});
 
 app.listen(port, (req, res) => {
-    console.log('Server is running at port ' + port);
+	console.log("Server is running at port " + port);
 });
